0cbe0158a90bd8e352800df647594ecc5d72d4fc
Moses Gbondo <gbondos@berea.edu>
2020-02-27 23:51:16 -0500

    First commit

---
+++ src/build/precache-manifest.e9b658ac98d30d9ae55a4e7c97dfbce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8404eb85a9b196c691e365808c3732d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83138755d8eab19bf385",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "310407bbc0fa506c5657",
+    "url": "/static/js/2.8655eca7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8655eca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83138755d8eab19bf385",
+    "url": "/static/js/main.86342fe6.chunk.js"
+  },
+  {
+    "revision": "fc6b0834b26c11c7b958",
+    "url": "/static/js/runtime-main.742f9f3b.js"
+  }
+]);
